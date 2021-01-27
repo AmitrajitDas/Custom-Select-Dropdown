533d7797672079aee77fcdb4cc0d9810d070bb1e
Amitrajit31 <amitrajitdas31@gmail.com>
2021-01-27 19:39:55 +0530

    added elements

--- select.js
+++ select.js
@@ -1,5 +1,21 @@
 export default class Select {
     constructor(element) {
         this.element = element;
+        this.options = element.querySelectorAll('option')
+        this.customElement = document.createElement('div')
+        this.labelElement = document.createElement('span')
+        this.optionsCustomElement = document.createElement('ul')
+        setupCustomElement(this)
+        element.after(this.customElement)
     }
+}
+
+function setupCustomElement(select) {
+    select.customElement.classList.add('custom-select-container')
+
+    select.labelElement.classList.add('custom-select-value')
+    select.customElement.classList.append(select.labelElement)
+
+    select.optionsCustomElement.classList.add('custom-select-options')
+     select.customElement.classList.append(select.optionsCustomElement)
 }
