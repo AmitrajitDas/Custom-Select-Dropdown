fc72b53ddce76cacfeee3251cc244a82e4704230
Amitrajit31 <amitrajitdas31@gmail.com>
2021-01-27 19:52:51 +0530

    added option elements to select options

--- select.js
+++ select.js
@@ -1,12 +1,16 @@
 export default class Select {
     constructor(element) {
         this.element = element;
-        this.options = element.querySelectorAll('option')
+        this.options = getFormattedOptions(element.querySelectorAll('option'))
         this.customElement = document.createElement('div')
         this.labelElement = document.createElement('span')
         this.optionsCustomElement = document.createElement('ul')
         setupCustomElement(this)
         element.after(this.customElement)
+    }
+
+    get selectedOption() {
+        return this.options.find(option => option.selected)
     }
 }
 
@@ -14,8 +18,27 @@ function setupCustomElement(select) {
     select.customElement.classList.add('custom-select-container')
 
     select.labelElement.classList.add('custom-select-value')
+    select.labelElement.innerText = select.selectedOption.value
     select.customElement.classList.append(select.labelElement)
 
     select.optionsCustomElement.classList.add('custom-select-options')
+    select.options.forEach(option => {
+        const optionElement = document.createElement('li')
+        optionElement.classList.add('custom-select-option')
+        optionElement.classList.toggle('selected', option.selected)
+        optionElement.innerText = option.label
+        optionElement.dataset.value = option.value
+    })
      select.customElement.classList.append(select.optionsCustomElement)
+}
+
+function getFormattedOptions(optionElements) {
+    [...optionElements].map(optionElement => {
+        return {
+            value: optionElement.value,
+            label: optionElement.label,
+            selected: optionElement.selected,
+            element: optionElement
+        }
+    })
 }
